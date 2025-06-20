d58c5b4ca701ebb1aa4630aba1e152a04b280400
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-06-20 15:40:04 +0530

    Add files via upload

---
+++ js/second.js
@@ -0,0 +1,15 @@
+ export function multiply(a,b,c){
+
+   return a*b*c;
+ }
+
+
+
+ export default function() {
+  console.log("Welcome to JS Modules!");
+}
+
+
+export const pi = 3.14; 
+
+
---
+++ js/third.js
@@ -0,0 +1,16 @@
+import { multiply } from "./second.js";
+
+const result = multiply(4,5,7);
+
+console.log(result);
+
+
+import welcome from './second.js';
+welcome();
+
+
+import { pi } from './second.js';
+console.log(pi);
+
+
+
