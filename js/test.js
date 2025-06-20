8f2905c6f8a10e95f485e6bfb76590472574a328
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-06-20 15:38:22 +0530

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,24 @@
+
+
+//  ES Modules 
+
+const countryName = "India";
+
+
+ const studentDetails = {
+  fullName: "Anjali Verma",
+  rollNo: 23,
+  course: "B.Tech CS"
+};
+
+
+ function squareNumber(num) {
+  return num * num;
+}
+
+
+ const favoriteColor = "Purple";
+
+
+
+ export {countryName,studentDetails,squareNumber,favoriteColor }
