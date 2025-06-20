d87906dba7e89fffd2cd7694007f2294cc0413c3
Natasha Bagde <124035587+bagdenatasha2001@users.noreply.github.com>
2025-06-20 15:34:28 +0530

    Add files via upload

---
+++ js/Main2.mjs
@@ -0,0 +1,34 @@
+
+// import
+
+import { A, B, C, D, E , add,fruits,arr,user,square,cube,mul} from './my_module.mjs';
+
+
+console.log(A);
+console.log(B);
+console.log(C);
+console.log(D);
+console.log(E);
+
+console.log(add);
+
+console.log(fruits);
+
+console.log(arr);
+
+
+console.log(user);
+console.log(`user name : ${user.name}`);
+console.log(`user age : ${user.age}`);
+console.log(`user phone : ${user.phone}`);
+
+
+console.log(square(5));
+console.log(cube(4));
+
+console.log(mul);
+
+
+ 
+
+
---
+++ js/my_module.mjs
@@ -0,0 +1,53 @@
+
+// this is a ES module in javascript.
+
+// export
+
+
+
+export  const A = 6 ;
+
+export  const  B = 3;
+
+export  const C = 2 ;
+
+export  const D = 5;
+
+export  const E = 4 ;
+
+const  add = A+B+C+D+E;
+const mul =  A*B*C*D*E;
+
+
+
+const fruits = ["Apple", "Banana", "Orange","Mango","Watermelon","Grapes"];
+
+const arr = ["Natasha","Mahi","Mohit","Ankita","Bhavi","Samar"];
+
+
+const user={
+    name: "Natasha Bagde",
+    age:23,
+    phone:"13256"
+}
+
+
+ function square(n){
+  return n*n;
+};
+
+
+
+ function  cube(n){
+  return n*n*n;
+};
+
+
+
+export {add,fruits ,arr, user,square,cube,mul};
+
+
+
+
+
+
